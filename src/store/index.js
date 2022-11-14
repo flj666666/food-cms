522926a065a428476e2ee39b87e54c0cebf790ad
CheMount <1543549093@qq.com>
2022-11-14 14:56:03 +0800

    全局守卫

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,7 @@ const vuexLocal = new VuexPersistence({
   },
 })
 
-// import users from '@/store/models/users'
+
 import message from '@/store/models/message'
 import manage from '@/store/models/manage'
 import activeManage from '@/store/models/activeManage'
